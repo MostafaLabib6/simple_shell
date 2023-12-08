0a0d5c5db61a96f695d5e10f9f281f43e3430e7f
mostafa <mostafa@example.com>
2023-12-08 20:24:13 +0200

    impleament getline functionality to return whole input line

---
+++ get_line.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * get_line - responsible for reading line of input from user.
+ * Return: The buffer containing user input.
+ */
+char *get_line()
+{
+	char *line;
+	size_t len;
+	ssize_t read;
+
+	line = NULL;
+	len = 0;
+
+	read = getline(&line, &len, stdin);
+	if (read == -1)
+	{
+		free(read);
+		exit(EXIT_SUCCESS);
+	}
+	return (read);
+}
