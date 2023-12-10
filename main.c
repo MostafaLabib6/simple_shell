e1063a0ed8ad45a594d4c924cf8690d9a846055e
mostafa <mostafa@example.com>
2023-12-10 12:13:28 +0200

    decoment main file

--- main.c
+++ main.c
@@ -1,40 +1,46 @@
 #include "shell.h"
 
+/**
+ * main - The program enters a continuous loop that
+ * represents the main shell execution.
+ * Return: 0 on success
+ */
+
 int main(void)
 {
-    char *line, env, file_name;
-    char **tokens;
-    int create_status;
-    struct stat buf;
+	char *line, env, file_name;
+	char **tokens;
+	int create_status;
+	struct stat buf;
 
-    while (1)
-    {
-        /*prints the prompt*/
-        prompt(buf);
-        line = get_line();
-        if (str_cmp(line, "\n", 1) == 1)
-        {
-            free(line);
-            continue;
-        }
-        /*generate token and the check if empty or not*/
-        tokens = str_toknize(line);
-        if (tokens[0] == NULL)
-        { /*means that the line is empty.*/
-            free(tokens);
-            continue;
-        }
+	while (1)
+	{
+		/*prints the prompt*/
+		prompt(buf);
+		line = get_line();
+		if (str_cmp(line, "\n", 1) == 1)
+		{
+			free(line);
+			continue;
+		}
+		/*generate token and the check if empty or not*/
+		tokens = str_toknize(line);
+		if (tokens[0] == NULL)
+		{ /*means that the line is empty.*/
+			free(tokens);
+			continue;
+		}
 
-        create_status = create_child(tokens[0], tokens);
-        if (create_status == -1)
-        {
-            printf("Createion error");
-        }
-        free(line);
-        free(env);
-        free(file_name);
-        free(buf);
-        free(create_status);
-        free(tokens);
-    }
+		create_status = create_child(tokens[0], tokens);
+		if (create_status == -1)
+		{
+			printf("Createion error");
+		}
+		free(line);
+		free(env);
+		free(file_name);
+		free(buf);
+		free(create_status);
+		free(tokens);
+	}
 }
