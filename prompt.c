d6c16877fdee21b4c17bd873a8899af581637b2c
mostafa <mostafa@example.com>
2023-12-10 12:09:42 +0200

    print prompt symbol if the file info is regular.

---
+++ prompt.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * prompt - check if passed file is regualar or not and print
+ * Prompt symbol if valid.
+ * @file_name: User input string.
+ * @st_buf: buffer that contains the information about file.
+ * Return: Pointer to an array of tokens.
+ */
+
+void prompt(struct stat st_buf)
+{
+	/*get information about the file*/
+	if (stat(STDIN_FILENO, &st_buf) != -1)
+	{
+		/*check if the file is regualr*/
+		if (S_ISREG(st_buf.st_mode))
+			_puts(PROMPT);
+	}
+}
